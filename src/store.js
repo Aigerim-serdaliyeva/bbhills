28d5cbdb69eeaf3e48010bcdf2139e66a9acb210
Sanzhar <s.shakhmerden@muratov.kz>
2019-07-24 17:20:01 +0600

    spa changes

--- src/store.js
+++ src/store.js
@@ -3,14 +3,10 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-import menuMobile from "./store/menuMobile";
-import menuDesktop from "./store/menuDesktop";
-
 const store = () =>
     new Vuex.Store({
         modules: {
-            menuMobile: menuMobile,
-            menuDesktop: menuDesktop
+            
         }
     });
 
--- vue.config.js
+++ vue.config.js
@@ -8,26 +8,3 @@ module.exports = {
         }
     }
 };
-
-// const imagemin = require('imagemin');
-// const imageminJpegtran = require('imagemin-jpegtran');
-// const imageminPngquant = require('imagemin-pngquant');
-// const imageminSvgo = require('imagemin-svgo');
-
-// (async () => {
-// 	const files = await imagemin(['src/assets/images/*.{jpg,png,svg}'], 'build/images', {
-// 		plugins: [
-// 			imageminJpegtran(),
-// 			imageminPngquant({
-// 				quality: [0.8, 1]
-//             }),
-//             imageminSvgo({
-//                 plugins: [
-//                     {removeViewBox: false}
-//                 ]
-//             })
-// 		]
-// 	});
-
-// 	console.log(files);
-// })();
